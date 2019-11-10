eda970081d8042528193bbd262aba09d78d94810
sanchjain <sanchjain101@gmail.com>
2019-11-10 12:09:47 +0530

    Django Files

--- DJANGO/hackathon/settings.py
+++ DJANGO/hackathon/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILE_DIRS = '/users/sanchitjain/documents/django/hackathon/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 GOOGLE_MAPS_API_KEY = 'AIzaSyCNZPxXdc5tWaYbOoFWCK2NHrOHlrxCtAk'
